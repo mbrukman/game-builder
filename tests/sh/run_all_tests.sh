24690851e8da0e27fbb41ca2211651e1a4759986
Misha Brukman <mbrukman@google.com>
2016-10-23 18:31:34 +0100

    Moved test runner code to shell script.

--- tests/sh/run_all_tests.sh
+++ tests/sh/run_all_tests.sh
@@ -1,19 +1,27 @@
-# Copyright 2009 Google Inc.
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
+#!/bin/bash
+#
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-SRC = ../../src
-include $(SRC)/Makefile.common
-
-test:
-	$(VERB) ./run_all_tests.sh
+for t in *_test.sh ; do
+  echo -n "Testing ${t} ... "
+  stdout="$(mktemp "/tmp/${t}.XXXXXX")"
+  "./${t}" > "${stdout}" 2>&1
+  if [[ -n "$(tail -n 1 "${stdout}" | grep PASSED)" ]]; then
+    rm -f "${stdout}"
+    echo "OK."
+  else
+    echo "failed, see log in ${stdout}"
+  fi
+done
